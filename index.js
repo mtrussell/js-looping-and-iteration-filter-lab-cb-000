8593e4e64f76c153bc8c4986ce2b9871f0d0ca39
Matt Trussell <smtrussell@gmail.com>
2018-11-01 19:10:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Code your solution in this file
 
-function findMatching(){
-  
+function findMatching(drivers, name){
+  const driverName = name.toLowerCase();
+  const matches = drivers.filter(driver, function(){
+    return driver.toLowerCase() === driverName;
+  });
 }
 
 function fuzzyMatch(){
